6b3301654c072f09a665ddca0ca1100deaa96e77
Miika Haapanen <123482448+theMsH@users.noreply.github.com>
2024-11-21 01:58:13 +0200

    Tehty klikattava employee card, joka ottaa parametrinä shiftresponse listan ja authuser-mockupin.

---
+++ src/assets/css/cardButton.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import { theme } from "./theme";
+
+
+export const CardButton = styled.button`
+    background-color: ${theme.componentBg};
+    display: flex;
+    flex-direction: column;
+    border: none;
+    border-radius: 10px;
+    width: 680px;
+    padding: 10px;
+    box-shadow: 0 3px 4px 0 rgba(0,0,0,0.2);
+    margin-bottom: 5px;
+`
