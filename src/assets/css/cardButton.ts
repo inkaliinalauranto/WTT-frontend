bdcd127dfd962ad7f906e0d5889f7293cd4ee333
Miika Haapanen <123482448+theMsH@users.noreply.github.com>
2024-11-23 02:10:25 +0200

    Luotu hover efekti

--- src/assets/css/cardButton.ts
+++ src/assets/css/cardButton.ts
@@ -4,6 +4,7 @@ import { theme } from "./theme";
 
 export const CardButton = styled.button`
     background-color: ${theme.componentBg};
+    width: 100%;
     display: flex;
     flex-direction: column;
     border: none;
@@ -11,4 +12,8 @@ export const CardButton = styled.button`
     padding: 10px;
     box-shadow: 0 3px 4px 0 rgba(0,0,0,0.2);
     margin-bottom: 5px;
+
+    &:hover {
+        box-shadow: inset 0 0 0 2px rgba(0,0,0,0.5);
+    }
 `
